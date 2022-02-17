2bf80db9a7bfd396bfe197f5cd4dd4d2be5aeca8
lonelysoul <62841189+lonelysoul@users.noreply.github.com>
2022-02-17 23:18:55 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,7 +11,7 @@
 #
 
 # Uncomment a feed source
-ehco 'src-git NueXini_Packages https://github.com/NueXini/NueXini_Packages.git' >> feeds.conf.default
+echo 'src-git NueXini_Packages https://github.com/NueXini/NueXini_Packages.git' >> feeds.conf.default
 
 
 # Add a feed source
