d687d9199a82eb35e726b22c865206b7b3b12b41
lonelysoul <62841189+lonelysoul@users.noreply.github.com>
2022-03-06 08:02:06 +0800

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -2,6 +2,7 @@
 cd openwrt
 git pull
 ./scripts/feeds update -a && ./scripts/feeds install -a
-make menuconfig
 make -j8 download
 make -j$(($(nproc) + 1)) V=s
+cd /home/openwrt/openwrt/bin/targets/x86/64
+./getindex.sh
